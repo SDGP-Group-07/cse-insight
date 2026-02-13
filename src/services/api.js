947cea02a753899e3c5ac7a9721468fc724fb091
Mohammed Shaahid <122220522+Shaahid02@users.noreply.github.com>
2026-02-13 21:49:21 +0530

    Update API base URL for the service

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 //const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000/api';
 const API_BASE_URL =
-  'https://cseinsight-api-f3bkbeebgmfaczg9.southeastasia-01.azurewebsites.net/api';
+  'https://cse-insight-api-btgbf9efd3g7fze2.southeastasia-01.azurewebsites.net/api';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
